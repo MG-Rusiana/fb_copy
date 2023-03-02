bbeccd425c5b4944ea4a56a0b7e9a2a4d5ba64cb
Mark Gil P. Rusiana <markgilrusiana437@gmail.com>
2023-03-02 17:24:34 +0800

    browser router changed to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 // import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Login } from './screens/login/login';
 import { Profile } from './screens/profile/profile';
 
 function App() {
   return(
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" index element={<Login />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 export default App;
