a2ad4ab0f3d130e4ef060f194c899545b1d3cc04
Mark Gil P. Rusiana <markgilrusiana437@gmail.com>
2023-03-07 19:52:12 +0800

    routing test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Login } from './screens/login/login';
 import { Profile } from './screens/profile/profile';
 
@@ -7,7 +7,8 @@ function App() {
   return(
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Navigate replace to="/fb_copy" />} />
+        <Route path="/fb_copy" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
